caa8cd456c82df04ecafcabe466a6505fc583c92
syaifulazham <syaiful.azham.yusoff@outlook.com>
2023-07-07 09:30:14 +0800

    update arena

--- arena.js
+++ arena.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
 const app = express();
 const path = require('path');
 
@@ -10,12 +13,66 @@ let __DATA__SCHEMA__ = 'techlympic';
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+//set cookies
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //console.log('directory name-path: ',path.join(__dirname, 'public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
+  next();
+});
+
+
 // Define a route for rendering the map page
 app.get('/', (req, res) => {
-  res.render('index');
+  try{
+    //var sessionId = mysession(req.cookies['connect.sid']);
+    var session = req.cookies['arenaId'];
+    console.log('my session :',session)
+    if(session.user.authorized){
+      res.render('index.ejs', { user: session.user, page: '__body.ejs' });
+    }else{
+      res.render('index.ejs', { user: {}, page: 'login.ejs' });
+    }
+
+    console.log('---------------->> ',session);
+  }catch(err){
+    //console.log(err);
+    res.render('index.ejs', { user: {}, page: 'login.ejs' });
+  }
+  
+  //res.render('index');
+});
+
+
+app.post('/api/user/login', (req, res)=>{
+  try{
+    uid = req.body.uid;
+    pwd = req.body.pwd;
+    api.user.login(uid, pwd, (user)=>{
+      if(user.authorized){
+        res.cookie('arenaId', {user:user});
+        console.log('Will be rendered to index===>')
+        res.send({authorized:true});
+      }else{
+        res.send(user);
+      }
+        
+    });
+  }catch(err){
+    console.log(err);
+    res.render('index.ejs', { user: {}, page: 'login.ejs' });
+  }
+
+});
+
+app.get('/logout', function (req, res, next) {
+  res.clearCookie('arenaId');
+  res.redirect('/');
 });
 
 // Start the server
--- routes/crud.js
+++ routes/crud.js
@@ -6,15 +6,24 @@ let __DATA__SCHEMA__ = 'techlympic';
 let API = {
     user:{
         login: (uid, pass, fn)=>{
+            //console.log('Login as: ',uid,pass,auth.auth()[__DATA__SCHEMA__]);
             if(uid=='' || pass=='') return 0;
             var con = mysql.createConnection(auth.auth()[__DATA__SCHEMA__]);
             try{
-                con.query("SELECT * from peserta WHERE kp = ? and peserta_password = AES_ENCRYPT(?,CONCAT(?,?))",[uid, pass, uid, auth._SECRET_], 
+                //"SELECT * from peserta WHERE kp = ? and peserta_password = AES_ENCRYPT(kp,CONCAT(?,?))
+                con.query("SELECT b.kodsekolah, b.namasekolah, b.peringkat, concat(alamat1,' ',alamat2,', ', poskod,' ', bandar,', ',negeri) alamat,a.* from peserta a left join user b using(usr_email) WHERE kp = ? and peserta_password = AES_ENCRYPT(?,CONCAT(kp,?))",[uid, pass, auth._SECRET_], 
                 function (err, result) {
+                    console.log('result ====> ', result);
                     if(result.length > 0){
                         var user = {
                             name: result[0].nama,
                             ic: result[0].kp,
+                            kodsekolah: result[0].kodsekolah,
+                            namasekolah: result[0].namasekolah,
+                            darjah_tingkatan: result[0].darjah_tingkatan,
+                            jantina: result[0].jantina,
+                            peringkat: result[0].peringkat,
+                            alamat: result[0].alamat,
                             avatar: ''
                         };
                         
